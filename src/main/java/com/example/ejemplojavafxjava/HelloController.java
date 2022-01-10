0d820705b18ec4b7773ac18aa8c213cd55c6b112
Carlos <carlostena@centroafuera.onmicrosoft.com>
2022-01-10 12:19:32 +0100

    Abriendo una sola venta

--- src/main/java/com/example/ejemplojavafxjava/HelloController.java
+++ src/main/java/com/example/ejemplojavafxjava/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.ejemplojavafxjava;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -14,21 +16,27 @@ public class HelloController {
     private Label welcomeText;
 
     private int contadorDePulsaciones = 0;
-
+    private Ventana2Controller v = null;
+    private Stage stage = null;
     @FXML
     protected void onHelloButtonClick() {
         try {
-            Stage stage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("ventana2.fxml"));
 
-            VBox root = (VBox) loader.load();
-            Scene scene = new Scene(root, 500, 500);
-            stage.setScene(scene);
-            stage.show();
+            if (stage == null || !stage.isShowing()) {
+                stage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("ventana2.fxml"));
 
-            Ventana2Controller v = loader.getController();
+                VBox root = (VBox) loader.load();
+                Scene scene = new Scene(root, 500, 500);
+                stage.setScene(scene);
+                stage.show();
+
+                v = loader.getController();
+            }
+
             contadorDePulsaciones++;
             v.funcionPublica(contadorDePulsaciones);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
